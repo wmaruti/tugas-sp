58090da54838255b6886235d645ee609f71c1292
Wahyu Maruti Adjie <32800189+wmaruti@users.noreply.github.com>
2018-08-21 14:22:58 +0700

    Create cek bilangan ganjil genap.py

---
+++ cek bilangan ganjil genap.py
@@ -0,0 +1,8 @@
+bilangan = input('Masukan bilangan: ')
+
+if bilangan % 2 == 0:
+    print "%i adalah bilangan genap" % bilangan
+else:
+    print "%i adalah bilangan ganjil" % bilangan
+
+# selesai
